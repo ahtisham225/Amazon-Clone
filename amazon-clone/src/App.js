4c568ff021c95ad02765f2c631fdbbf77dad7480
ahtisham225 <ahtisham225ali@gmail.com>
2023-04-14 16:12:25 +0500

    Routes set up

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,12 +1,33 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom" 
 function App() {
   return (
-    <div className = "App">
-      <h1> Come let's Build Amazon</h1>
-    </div>
+    <Router>
+      <div className = "App">
+      <Routes>
+        <Route path = "/checkout" element = {<Checkout />}>
+          
+        </Route>
+        <Route path = "/login" element = {<Login />}>
+        </Route>
+        <Route path = "/" element = {<Home/>}>
+        </Route>
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
+function Home(){
+  return <h1>Welcome to HomePage</h1>
+}
+
+function Login(){
+  return <h1>Welcome to Login Page</h1>
+}
+
+function Checkout(){
+  return <h1>Welcome to Checkout Page</h1>
+}
 export default App;
