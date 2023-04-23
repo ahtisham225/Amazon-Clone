15ce60f677acb16780efa2afc6e3554caa800bff
ahtisham225 <ahtisham225ali@gmail.com>
2023-04-24 02:44:36 +0500

    Login Page and Final Touches

---
+++ amazon-clone/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+function Footer(){
+    return (
+        <div className='footer'>
+            Thank you for taking the time to visit our website. 
+            We are currently in the process of updating and improving our site to enhance your experience. While our site is under construction, we apologize for any inconvenience this may cause. Please check back with us in the near future, as we will have a brand new and improved website for you to explore. Thank you for your patience and understanding.
+        </div>
+    )
+}
+export default Footer;
